493a628144ee31ee76685e720c641246d6d4cf1f
JOHAN\NamNguyen <nguyenthanhnam.6663@gmail.com>
2025-05-15 09:56:20 +0700

    Fix: move class-validator to dependencies

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Transform, Type } from 'class-transformer';
-import { IsNumber } from 'class-validator';
+import { IsNumber, IsOptional } from 'class-validator';
 
 export class CreateProductDto {
   @ApiProperty()
