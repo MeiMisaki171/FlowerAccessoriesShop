638aad37844fe4087151eabace40bea3d543ed46
JOHAN\NamNguyen <nguyenthanhnam.6663@gmail.com>
2025-05-15 09:50:02 +0700

    deplot vercel

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Transform, Type } from 'class-transformer';
-import { IsNumber, IsOptional } from 'class-validator';
+import { IsNumber } from 'class-validator';
 
 export class CreateProductDto {
   @ApiProperty()
